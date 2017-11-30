fc458e9029461796b13e979ab75463913d12c092
shanhui <shanhui@agora.io>
2017-11-30 13:03:06 +0800

    update ip2int

--- ip_int_trans.py
+++ ip_int_trans.py
@@ -4,15 +4,24 @@ import workflow
 
 
 def ip2int(value):
-    return str(socket.ntohl(struct.unpack("I",socket.inet_aton(str(value)))[0]))
+    return str(socket.ntohl(struct.unpack("I", socket.inet_aton(str(value)))[0]))
 
 
-def int2ip(value):
-     return socket.inet_ntoa(struct.pack('I',socket.htonl(value)))
+def int2ip(value, htonl=True):
+    if htonl:
+        return socket.inet_ntoa(struct.pack('I', socket.htonl(value)))
+    else:
+        return socket.inet_ntoa(struct.pack('I', value))
 
 
 def query(param):
     wf = workflow.Workflow()
+    try:
+        ip = int2ip(int(param), False)
+        wf.add_item(title=ip, subtitle='int2ip', arg=ip, valid=True)
+    except:
+        pass
+
     try:
         wf.add_item(title=int2ip(int(param)), subtitle='int2ip',
                     arg=int2ip(int(param)), valid=True)
